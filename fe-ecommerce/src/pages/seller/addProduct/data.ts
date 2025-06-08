3dd8a4a51c50f732569cda52c45d26c0bd26b85e
sairam.cpwt <sairam.cpwt@gmail.com>
2025-06-08 17:05:42 +0530

    Add Product UI completed

---
+++ fe-ecommerce/src/pages/seller/addProduct/data.ts
@@ -0,0 +1,26 @@
+export const categorys = [
+  {
+    id: 1,
+    name: "Sports",
+  },
+  {
+    id: 2,
+    name: "Tshirt",
+  },
+  {
+    id: 3,
+    name: "Mobile",
+  },
+  {
+    id: 4,
+    name: "Computer",
+  },
+  {
+    id: 5,
+    name: "Watch",
+  },
+  {
+    id: 6,
+    name: "Pant",
+  },
+];
